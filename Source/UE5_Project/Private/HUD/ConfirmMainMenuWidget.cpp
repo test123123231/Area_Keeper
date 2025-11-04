abd6a5d533d29c46c76e1789f0906193eb793f1f
Syud0 <nosiwoo0421@gmail.com>
2025-11-05 07:43:05 +0900

    feature/#58 ConfrimMainMenuWIdget UI 클래스 생성

---
+++ Source/UE5_Project/Private/HUD/ConfirmMainMenuWidget.cpp
@@ -0,0 +1,79 @@
+﻿#include "HUD/ConfirmMainMenuWidget.h"
+#include "Components/Button.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void UConfirmMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// 플레이어 컨트롤러 캐시
+	PlayerControllerRef = GetOwningPlayer();
+
+	// 버튼 바인딩
+	if (Button_Yes)
+	{
+		Button_Yes->OnClicked.AddDynamic(this, &UConfirmMainMenuWidget::OnYesClicked);
+	}
+	if (Button_No)
+	{
+		Button_No->OnClicked.AddDynamic(this, &UConfirmMainMenuWidget::OnNoClicked);
+	}
+}
+
+
+// 이 위젯을 연 부모 메뉴(PauseMenu)를 설정
+void UConfirmMainMenuWidget::SetParentMenu(UUserWidget* InParentMenu)
+{
+	ParentMenu = InParentMenu;
+}
+
+
+// '예' (메인 메뉴로 이동) 버튼 클릭 시
+void UConfirmMainMenuWidget::OnYesClicked()
+{
+	if (PlayerControllerRef)
+	{
+		// 게임 일시정지를 해제
+		PlayerControllerRef->SetPause(false);
+
+		// 입력 모드를 게임 전용으로 되돌리고 마우스를 숨김
+		FInputModeGameOnly InputModeData;
+		PlayerControllerRef->SetInputMode(InputModeData);
+		PlayerControllerRef->bShowMouseCursor = false;
+	}
+
+	// 메인 메뉴 레벨을 로드
+	UGameplayStatics::OpenLevel(this, MainMenuLevelName);
+}
+
+
+// '아니오' (취소) 버튼 클릭 시
+void UConfirmMainMenuWidget::OnNoClicked()
+{
+	CloseMenu();
+}
+
+
+// 위젯을 닫고 부모 메뉴로 복귀
+void UConfirmMainMenuWidget::CloseMenu()
+{
+	// 부모 메뉴(PauseMenu)가 있다면 다시 보이게 함
+	if (ParentMenu)
+	{
+		ParentMenu->SetVisibility(ESlateVisibility::Visible);
+
+		// 부모 위젯(PauseMenu)으로 돌아가기 위해 GameAndUI 모드로 '복원'
+		if (PlayerControllerRef)
+		{
+			FInputModeGameAndUI InputModeData;
+			InputModeData.SetWidgetToFocus(ParentMenu->TakeWidget());
+			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::LockInFullscreen);
+			PlayerControllerRef->SetInputMode(InputModeData);
+		}
+	}
+
+	// 자신을 뷰포트에서 제거
+	RemoveFromParent();
+}
---
+++ Source/UE5_Project/Public/HUD/ConfirmMainMenuWidget.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Input/Events.h"
+#include "Input/Reply.h"
+#include "ConfirmMainMenuWidget.generated.h"
+
+
+class UButton;
+class APlayerController;
+
+
+/**
+ * '메인 메뉴로 돌아가기' 확인 팝업 (WBP_ConfirmMainMenu)의 C++ 기반 클래스
+ * PauseMenuWidget에 의해 생성됩니다.
+ */
+UCLASS()
+class UE5_PROJECT_API UConfirmMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	// 이 위젯을 연 부모 메뉴(PauseMenu)를 설정
+	void SetParentMenu(UUserWidget* InParentMenu);
+
+protected:
+	// (EventConstruct) 위젯을 포커스 가능하게 설정하고 버튼을 바인딩
+	virtual void NativeConstruct() override;
+
+private:
+	//--- BP 위젯 변수 바인딩 ---
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Yes;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_No;
+
+	//--- C++ 이벤트 핸들러 ---
+
+	// '예' (메인 메뉴로 이동) 버튼 클릭 시 호출
+	UFUNCTION()
+	void OnYesClicked();
+
+	// '아니오' (취소) 버튼 클릭 시 호출
+	UFUNCTION()
+	void OnNoClicked();
+
+	/**
+	 * 이 위젯을 닫고 부모 메뉴(PauseMenu)로 포커스를 돌려줌
+	 */
+	void CloseMenu();
+
+	//--- 설정 ---
+
+	// BP의 클래스 기본값에서 설정할 메인 메뉴 레벨의 이름
+	UPROPERTY(EditDefaultsOnly, Category = "Config")
+	FName MainMenuLevelName = FName(TEXT("MainMenu"));
+
+	//--- 캐시된 참조 ---
+	UPROPERTY()
+	TObjectPtr<UUserWidget> ParentMenu;
+
+	UPROPERTY()
+	TObjectPtr<APlayerController> PlayerControllerRef;
+};
